38e0b9b1c6a60ee8134ec43c0964e6e584550cc0
Akhilesh Yadav - PIPL - MUM <akhileshy@jmbaxigrp.com>
2024-03-07 17:28:39 +0530

    .

--- src/main/java/com/ganeshaa/leetcode/stringg/S7_49.java
+++ src/main/java/com/ganeshaa/leetcode/stringg/S7_49.java
@@ -1,5 +1,8 @@
 package com.ganeshaa.leetcode.stringg;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class S7_49 {
@@ -10,6 +13,37 @@ public class S7_49 {
     }
 
     public List<List<String>> groupAnagrams(String[] strs) {
-        return null;
+        List<List<String>> outer = new ArrayList<>();
+        String stri = "";
+        String strj = "";
+        String strip = "";
+        String strjp = "";
+        for (int i = 0; i < strs.length; i++) {
+            List<String> inner = new ArrayList<>();
+            for (int j = i + 1; j < strs.length; j++) {
+
+                strip = strs[i];
+                strjp = strs[j];
+                char[] ci = strip.toCharArray();
+                char[] cj = strjp.toCharArray();
+
+                Arrays.sort(ci);
+                Arrays.sort(cj);
+
+                stri = String.valueOf(ci);
+                strj = String.valueOf(cj);
+
+                System.out.println("stri = " + stri);
+                System.out.println("strj = " + strj);
+                if (stri.equals(strj)) {
+                    inner.add(strj);
+                }
+            }
+            inner.add(stri);
+            outer.add(inner);
+            inner.clear();
+            System.out.println("====================");
+        }
+        return outer;
     }
 }
--- src/main/java/com/ganeshaa/leetcode/stringg/Testt.java
+++ src/main/java/com/ganeshaa/leetcode/stringg/Testt.java
@@ -1,15 +1,30 @@
 package com.ganeshaa.leetcode.stringg;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Testt {
     public static void main(String[] args) {
 
-//        String name = "alice and bob love leetcode";
-//        for (int i=0; i<name.length(); i++){
-//            char charAt = name.charAt(i);
-//            System.out.println("charAt = " + (int)charAt);
-//        }
-
-        String str = "This1";
+        String name = "akhilesh";
+        System.out.println("name = " + name);
+        char[] charArray = name.toCharArray();
+        Arrays.sort(charArray);
+        String string = String.valueOf(charArray);
+        System.out.println("string = " + string);
+//        List<Integer> inner = new ArrayList<>();
+//        List<Integer> inner1 = new ArrayList<>();
+//        List<Integer> inner2 = new ArrayList<>();
+//        List<Integer> inner3 = new ArrayList<>();
+//        List<Integer> inner4 = new ArrayList<>();
+//        List<List<Integer>> outer = new ArrayList<>();
+//        outer.add(inner1);
+//        outer.add(inner2);
+//        outer.add(inner3);
+//        outer.add(inner4);
+//
+//        System.out.println(outer);
 
 
     }
