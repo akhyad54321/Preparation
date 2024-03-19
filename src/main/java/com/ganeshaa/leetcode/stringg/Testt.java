5b320a033db9cf80621683c3ec9d674f6aa5469b
Akhilesh Yadav - PIPL - MUM <akhileshy@jmbaxigrp.com>
2024-03-19 18:26:57 +0530

    changes done in Testt class

--- src/main/java/com/ganeshaa/leetcode/stringg/Testt.java
+++ src/main/java/com/ganeshaa/leetcode/stringg/Testt.java
@@ -1,14 +1,10 @@
 package com.ganeshaa.leetcode.stringg;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Testt {
     public static void main(String[] args) {
         String[] str = new String[5];
         str[2] = "a";
-        for (String s: str){
+        for (String s : str) {
             System.out.println(s);
         }
 
