7597f31752e0328714f75c120bafda100aeb6442
Akhilesh Yadav - PIPL - MUM <akhileshy@jmbaxigrp.com>
2024-03-29 18:50:23 +0530

    .

---
+++ src/main/java/com/ganeshaa/leetcode/arrayss/binarysearch/BS2_744.java
@@ -0,0 +1,12 @@
+package com.ganeshaa.leetcode.arrayss.binarysearch;
+
+public class BS2_744 {
+    public static void main(String[] args) {
+        BS2_744 app = new BS2_744();
+        char nextGreatestLetter = app.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'a');
+        System.out.println("nextGreatestLetter = " + nextGreatestLetter);
+    }
+    public char nextGreatestLetter(char[] letters, char target) {
+        return 'a';
+    }
+}
